8c383c6c17d779bcaa88ba41f3539e69884bdbcb
tanishqa-lalwani <43907446+tanishqa-lalwani@users.noreply.github.com>
2018-10-21 18:42:49 +0530

    check if the matrix is upper triangular matrix or not

---
+++ check_upper_matrix.c
@@ -0,0 +1,58 @@
+/******************************************************************************
+
+                            Find the given matrix is upper matrix or not
+
+*******************************************************************************/
+
+#include <stdio.h>
+void to_check_upper_matrix(int a[][10],int m);
+int i,j;
+int main()
+{
+    int a[10][10];
+    int m,n;
+    printf("Enter number of rows and columns of array");
+    scanf("%d",&m);
+    scanf("%d",&n);
+
+if(m!=n)
+{
+    printf("The given matrix is not square matrix");
+}
+    else 
+    {
+    for(i=0;i<m;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    
+    to_check_upper_matrix(a,m);
+    }
+    
+    return 0;
+}
+
+void to_check_upper_matrix(int a[][10],int m)    
+{
+    int it_is_upper=1;
+    for(i=0;i<m;i++)
+    {
+        for(j=0;j<m;j++)
+        {
+            if(i>j && a[i][j]!=0)
+            it_is_upper=0;
+        }
+    }
+    
+    if(it_is_upper==1)
+    {
+        printf("It is upper triangular matrix");
+        
+    }
+   else
+   printf("It is not");
+}
+
