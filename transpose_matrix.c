133f05dfc4a659441067f3e0a1ec02b14d725597
tanishqa-lalwani <43907446+tanishqa-lalwani@users.noreply.github.com>
2018-10-23 16:34:30 +0530

    updated transpose matrix

--- transpose_matrix.c
+++ transpose_matrix.c
@@ -1,34 +1,34 @@
 #include<stdio.h>
 int main()
 {
-int a[50][50];
-int i,j;
-
-printf("Enter rows");
-int m,n;
-scanf("%d",&m);
-printf("Enter columns");
-scanf("%d",&n);
-int t;
-
-printf("Enter elements");
-for(i=0;i<m;i++)
-{
-for(j=0;j<n;j++)
-{
-printf("Enter element [%d,%d] : ",i,j);
-scanf("%d",&a[i][j]);
-}
-}
-
-for(i=0;i<m;i++)
-{
-for(j=0;j<n;j++)
-{
-t=a[i][j];
-a[i][j]=a[j][i];
-a[j][i]=t;
-}
+        int n,m,i,j;
+        printf("\n Enter the rows and columns of matrix\n");
+        scanf("%d %d",&n,&m);
+        int a[n][m],b[m][n];
+        for(i=0;i<n;i++)
+        {
+                for(j=0;j<m;j++)
+                {
+                        scanf("%d",&a[i][j]);
+                }
+        }
+         for(i=0;i<n;i++)
+        {
+                for(j=0;j<m;j++)
+                {
+                       b[j][i]=a[i][j];
+                }
+        }
+        printf("\n Transpose of given matrix is\n");
+         for(i=0;i<m;i++)
+        {
+        printf("\n");
+                for(j=0;j<n;j++)
+                {
+                     printf("%d",b[i][j]);
+                }
+        }
+        
 }
 
 printf("New matrix is");
